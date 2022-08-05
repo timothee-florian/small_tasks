c712e7c55bd23e1719688e372e2d9b57876534f6
timothee-florian <timothee.florian.bronner@gmail.com>
2022-08-05 18:55:02 +0200

    simple example

---
+++ ml/tree_plotting.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+
+
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+# load dataset
+pima = pd.read_csv("diabetes.csv", header=1, names=col_names)
+
+feature_cols = ['pregnant', 'insulin', 'bmi', 'age','glucose','bp','pedigree']
+X = pima[feature_cols] # Features
+y = pima.label # Target variable
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
+
+clf = DecisionTreeClassifier(max_depth=3)
+
+# Train Decision Tree Classifer
+clf = clf.fit(X_train,y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+# fn=['sepal length (cm)','sepal width (cm)','petal length (cm)','petal width (cm)']
+# cn=['setosa', 'versicolor', 'virginica']
+fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=300)
+tree.plot_tree(clf,
+               feature_names = feature_cols, 
+               class_names=['0','1'],
+               filled = True);
+fig.savefig('imagename.png')
