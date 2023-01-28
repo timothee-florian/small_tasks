62e66072f45cdf812de8fb0622481f7937ff06bd
timothee.florian <timothee.florian.bronner@gmail.com>
2023-01-28 23:04:01 +0100

    allow sql scrip in dif folder than code

--- sql_automation/merge_multiple_create_view.py
+++ sql_automation/merge_multiple_create_view.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def clean_write(in_file, out_file, writing_mode):
     with open(file, 'r') as f:
@@ -8,6 +9,9 @@ def clean_write(in_file, out_file, writing_mode):
     with open(out_file, writing_mode) as f:
         f.write(c_code)
 
+folder = r'/home'
+files = ['first_view.sql', 'second_view.sql']
+files_code = map(lambda x: os.path.join(folder, x), files)
 out_file = 'views.sql'
 with open(out_file, 'w') as f:
     f.write('')
